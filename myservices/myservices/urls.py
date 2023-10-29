3beb62eccb8ec00692491201ea1ca12bf14cd73f
GregFilth <gregfilth@gmail.com>
2023-10-29 12:59:15 +0100

    Changes to be committed: 	modified:   myservices/myservices/__pycache__/urls.cpython-311.pyc 	modified:   myservices/myservices/urls.py

--- myservices/myservices/urls.py
+++ myservices/myservices/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('partyservice/', include("partyservice.urls"))
+    path('partyservice/', include("partyservice.urls")),
+    path('', include("partyservice.urls")),
 ]
